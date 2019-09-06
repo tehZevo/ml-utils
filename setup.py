c24906b58cd73d1c17fc3deeeba7c9befef19de6
tehZevo <tehZevo@users.noreply.github.com>
2019-09-05 22:17:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='ml_utils',
   version='0.1.0',
   install_requires = [
-    'matplotlib'
+    'matplotlib',
     'tensorflow',
     'numpy'
   ],
